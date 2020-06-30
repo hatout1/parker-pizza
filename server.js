e5f9f32b96a52f127f4f66fd6488ffb6d9e6126f
hatout01 <hatout1@gmail.com>
2020-06-29 20:36:01 -0700

    Added essential modules

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+app.use(cookieParser());
+app.use(express.json());
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/pathFinder";
+
+mongoose.connect(
+  MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+  },
+  () => {
+    console.log("successfully connect to db");
+  }
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const userRouter = require("./routes/User");
+app.use("/", userRouter);
+
+const postsRoutes = require("./routes/postsRoutes");
+app.use("/api", postsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`);
+});
