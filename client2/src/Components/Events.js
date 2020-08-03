db22a037a1f1dfcf5d683ed32401617363ccec2a
hatout01 <hatout1@gmail.com>
2020-08-02 19:56:03 -0700

    fixed heroku deploying issue

--- client2/src/Components/Events.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Events() {
-  return <div></div>;
-}
-
-export default Events;
--- client2/src/Components/Operations/OrderRequest.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function OrderRequest() {
-  return <div></div>;
-}
-
-export default OrderRequest;
